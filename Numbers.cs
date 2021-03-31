e06ff16aa46642d2fc90027e37d2825ca7a23b2f
lucasraditya125 <lucasraditya125@gmail.com>
2021-03-31 19:40:26 +0700

    Overloading Method

---
+++ Numbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+   public class Numbers
+    {
+        public int FindMinimum(int a, int b)
+        {
+            return a + b;
+        }
+        public int FindMinimum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        public int FindMaximum(int a, int b)
+        {
+            return a * b;
+        }
+        public int FindMaximum(int a, int b, int c)
+        {
+            return a * b - c;
+        }
+    }
+}
